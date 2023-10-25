5953546c2668421cd17e630ddb5f28ce5d208cb3
Ichsan Sandypratama <ichsan2668@gmail.com>
2023-10-25 18:18:57 +0700

    Add auth endpoint

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_231_025_063_020) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_25_095418) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'cars', force: :cascade do |t|
-    t.string 'model'
-    t.integer 'year'
-    t.string 'picture'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "cars", force: :cascade do |t|
+    t.string "model"
+    t.integer "year"
+    t.string "picture"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "name", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "jti"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["jti"], name: "index_users_on_jti"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
 end
